c85e6ccbb1c59c2f55ac078d35b177d454cabea4
manno <manno@manno.nl>
2025-03-03 19:29:45 +0100

    Changed start to Awake
    Made sure that there is a reference to the image of the stats bar as early as possible.
    Original code generated an error most likely. Oops

--- 1H-ClassProjects/Assets/Scripts/StatsBar.cs
+++ 1H-ClassProjects/Assets/Scripts/StatsBar.cs
@@ -15,7 +15,7 @@ public class StatsBar : MonoBehaviour
     [SerializeField] private Gradient colors;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         statsBar = GetComponent<Image>();
     }
